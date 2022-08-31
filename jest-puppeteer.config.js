afd05724b8a89af847fbe0644bcc4431301c8c44
stevenlinde <stevenlinde@google.com>
2022-08-31 16:34:35 -0700

    removing http-server and using webpack dev server

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -4,10 +4,10 @@
 STATIC=process.env.npm_package_config_dir || "docroot";
 PORT=process.env.npm_package_config_e2e_port || 8080;
 
-
+// TODO tell jest that e2e tests involve any bundle changes
 module.exports = {
     server: {
-        command: `http-server ${STATIC} -p ${PORT}`,
+        command: `npx webpack serve --static=${STATIC} --port=${PORT}`,
         usedPortAction: 'kill',
     },
 }
