b0ef46beb5fae3850b4cf34a9fda7315971453a9
stevenlinde <stevenlinde@google.com>
2022-09-09 22:53:51 -0700

    added babel finaly

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,16 @@ module.exports = {
   module : {
     rules: [
       { test: /\.css$/i, use: ["style-loader", "css-loader"],},
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
     ],
   },
   output: {
